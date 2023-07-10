48940c327c7b0f53ca4ab77321d0d751b386a6c1
steve zhang <871506263@qq.com>
2023-07-11 00:48:29 +0800

    1

--- bin/autoDeploy.sh
+++ bin/autoDeploy.sh
@@ -4,7 +4,7 @@ cd docs/.vitepress/dist
 
 git init
 git add -A
-git commit -m "auto construct blog"
+git commit -m "自动部署到github"
 
 git push -f https://github.com/jay6697117/jay6697117.github.io.git master:gh-pages
 cd ../
