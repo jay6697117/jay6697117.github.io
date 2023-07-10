a376e27347d293f3293ebd4278c540f1fa085e36
steve zhang <871506263@qq.com>
2023-07-11 00:53:40 +0800

    自动部署到github: master分支

--- bin/autoDeploy.sh
+++ bin/autoDeploy.sh
@@ -1,7 +1,7 @@
 pnpm i
 git init
 git add -A
-git commit -m "自动部署到github: root目录"
+git commit -m "自动部署到github: master分支"
 git push -f
 
 
@@ -10,7 +10,7 @@ cd docs/.vitepress/dist
 
 git init
 git add -A
-git commit -m "自动部署到github: dist目录"
+git commit -m "自动部署到github: gh-pages分支"
 
 git push -f https://github.com/jay6697117/jay6697117.github.io.git master:gh-pages
 cd ../
